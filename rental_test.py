57c07dae4bf555d1e7199ce66f547cc1b623019e
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-20 19:20:01 +0700

    Update test for Rental

--- rental_test.py
+++ rental_test.py
@@ -1,31 +1,33 @@
 import unittest
 from rental import Rental
 from movie import Movie
+from pricing import NEW_RELEASE, REGULAR, CHILDRENS
 
 
 class RentalTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_movie = Movie("Dune: Part Two", Movie.NEW_RELEASE)
-        self.regular_movie = Movie("Air", Movie.REGULAR)
-        self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
+        self.new_movie = Movie("Dune: Part Two", 2024, {"Sci-Fi"})
+        self.regular_movie = Movie("Air", 2023, {"Drama"})
+        self.childrens_movie = Movie("Frozen", 2013, {"Children"})
 
     def test_movie_attributes(self):
-        """trivial test to catch refactoring errors or change in API of Movie"""
-        m = Movie("Air", Movie.REGULAR)
-        self.assertEqual("Air", m.get_title())
-        self.assertEqual(Movie.REGULAR, m.get_price_code())
+        self.assertEqual("Dune: Part Two", self.new_movie.title)
+        self.assertEqual(NEW_RELEASE, self.new_movie.price_code_for_movie())
+        self.assertEqual("Air", self.regular_movie.title)
+        self.assertEqual(REGULAR, self.regular_movie.price_code_for_movie())
+        self.assertEqual("Frozen", self.childrens_movie.title)
+        self.assertEqual(CHILDRENS, self.childrens_movie.price_code_for_movie())
 
     def test_rental_price(self):
-        """test to get a rental price of each price code"""
         rental = Rental(self.new_movie, 1)
         self.assertEqual(rental.get_price(), 3.0)
         rental = Rental(self.new_movie, 5)
         self.assertEqual(rental.get_price(), 15.0)
 
         rental = Rental(self.regular_movie, 2)
-        self.assertEqual(rental.get_price(), 2)
-        rental = Rental(self.regular_movie, 4.0)
+        self.assertEqual(rental.get_price(), 2.0)
+        rental = Rental(self.regular_movie, 4)
         self.assertEqual(rental.get_price(), 5.0)
 
         rental = Rental(self.childrens_movie, 3)
@@ -34,7 +36,6 @@ class RentalTest(unittest.TestCase):
         self.assertEqual(rental.get_price(), 4.5)
 
     def test_rental_points(self):
-        """test to get a frequent renter points of each price code"""
         rental = Rental(self.new_movie, 1)
         self.assertEqual(rental.rental_points(), 1)
         rental = Rental(self.new_movie, 4)
@@ -49,3 +50,7 @@ class RentalTest(unittest.TestCase):
         self.assertEqual(rental.rental_points(), 1)
         rental = Rental(self.childrens_movie, 4)
         self.assertEqual(rental.rental_points(), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
