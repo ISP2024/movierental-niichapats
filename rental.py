f47aecfc026cf831d61e1ddd4709a3f3fce7607a
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-20 18:56:46 +0700

    Modify Rental class to determine the Price Code itself

--- rental.py
+++ rental.py
@@ -9,13 +9,13 @@ class Rental:
     For simplicity of this application only days_rented is recorded.
     """
 
-    def __init__(self, movie, price_code, days_rented):
+    def __init__(self, movie, days_rented):
         """Initialize a new movie rental object for
            a movie with known rental period (daysRented).
         """
         self.movie = movie
         self.days_rented = days_rented
-        self.price_code = price_code
+        self.price_code = movie.price_code_for_movie()
 
     def get_movie(self):
         return self.movie
@@ -31,3 +31,4 @@ class Rental:
 
     def rental_points(self):
         return self.price_code.get_rental_points(self.days_rented)
+
